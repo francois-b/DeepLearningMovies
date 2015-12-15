ce83eac3f4e266ccfafd850b79ae99997d8845ab
Francois <f.bouet@gmail.com>
2015-12-15 01:24:11 +0100

    Improve readability, output
    Disable BS4 warnings, rename reviews to articles to make the code more
    reusable, and document the functions.

--- train.py
+++ train.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-#  Author: Angela Chapman
-#  Date: 8/6/2014
-#
-#  This file contains code to accompany the Kaggle tutorial
-#  "Deep learning goes to the movies".  The code in this file
-#  is for Parts 2 and 3 of the tutorial, which cover how to
-#  train a model using Word2Vec.
-#
-# *************************************** #
+"""Training code using Word2Vec.
+
+This code is based on Angela Chapman's tutorial on Kaggle:
+
+https://www.kaggle.com/c/word2vec-nlp-tutorial
+
+"""
 
 # ****** Read the two training sets and the test set
 
@@ -23,64 +21,80 @@ from sklearn.ensemble import RandomForestClassifier
 
 from utils import review_to_wordlist, review_to_sentences
 
+# Beautiful Soup throws a lot of benign warnings which we will ignore
+import warnings
+warnings.filterwarnings("ignore")
+
 
 # ****** Define functions to create average word vectors
 
 
-def makeFeatureVec(words, model, num_features):
-    """Average all of the word vectors in a given paragraph."""
+def make_feature_vec(articles_words, model, num_features):
+    """Average all of the word vectors in a given paragraph.
+
+    articles_words: list of words (strings)
+    model: instance of Word2Vec()
+    num_features: word vector dimensionality
+    """
 
     # Pre-initialize an empty numpy array (for speed)
-    featureVec = np.zeros((num_features,), dtype="float32")
+    feature_vec = np.zeros((num_features,), dtype="float32")
 
-    nwords = 0.0
+    nwords = 0
 
-    # Index2word is a list that contains the names of the words in
-    # the model's vocabulary. Convert it to a set, for speed
+    # index2word is a list that contains the names of the words in
+    # the model's vocabulary. Convert it to a set, for speed.
     index2word_set = set(model.index2word)
 
     # Loop over each word in the review and, if it is in the model's
-    # vocaublary, add its feature vector to the total
-    for word in words:
+    # vocabulary, add its feature vector to the total
+    for word in articles_words:
         if word in index2word_set:
-            nwords = nwords + 1.
-            featureVec = np.add(featureVec,model[word])
+            nwords = nwords + 1
+            feature_vec = np.add(feature_vec, model[word])
 
     # Divide the result by the number of words to get the average
-    featureVec = np.divide(featureVec,nwords)
-    return featureVec
+    feature_vec = np.divide(feature_vec, nwords)
+    return feature_vec
 
 
-def getAvgFeatureVecs(reviews, model, num_features):
-    """Given a set of reviews (each one a list of words), calculate
-    the average feature vector for each one and return a 2D numpy array."""
+def get_avg_feature_vecs(articles, model, num_features):
+    """Given a set of articles (each one a list of words), calculate
+    the average feature vector for each one and return a 2D numpy array.
+
+    articles: list of list of words (strings)
+    model: instance of Word2Vec()
+    num_features: word vector dimensionality
+    """
 
     # Initialize a counter
-    counter = 0.0
+    counter = 0
 
-    # Preallocate a 2D numpy array, for speed
-    reviewFeatureVecs = np.zeros((len(reviews), num_features), dtype="float32")
+    # Pre-allocate a 2D numpy array, for speed
+    reviewFeatureVecs = np.zeros((len(articles), num_features), dtype="float32")
 
-    # Loop through the reviews
-    for review in reviews:
+    # Loop through the articles
+    for review in articles:
 
        # Print a status message every 1000th review
-       if counter%1000. == 0.0:
-           print "Review %d of %d" % (counter, len(reviews))
+       if counter % 1000 == 0:
+           print "Review %d of %d" % (counter, len(articles))
 
        # Call the function (defined above) that makes average feature vectors
-       reviewFeatureVecs[counter] = makeFeatureVec(review, model, num_features)
+       reviewFeatureVecs[counter] = make_feature_vec(review, model, num_features)
 
        # Increment the counter
-       counter = counter + 1.
+       counter = counter + 1
     return reviewFeatureVecs
 
 
-def getCleanReviews(reviews):
+def get_clean_reviews(articles):
+    """Return a list of list of words."""
+
     clean_reviews = []
-    for review in reviews["review"]:
-        clean_reviews.append(review_to_wordlist(review,
-                                                       remove_stopwords=True))
+    for review_text in articles["review"]:
+        clean_reviews.append(review_to_wordlist(review_text,
+                                                remove_stopwords=True))
     return clean_reviews
 
 
@@ -113,12 +127,12 @@ if __name__ == '__main__':
     sentences = []  # Initialize an empty list of sentences
 
     print "Parsing sentences from training set"
-    for review in train["review"]:
-        sentences += review_to_sentences(review, tokenizer)
+    for review_text in train["review"]:
+        sentences += review_to_sentences(review_text, tokenizer)
 
     print "Parsing sentences from unlabeled set"
-    for review in unlabeled_train["review"]:
-        sentences += review_to_sentences(review, tokenizer)
+    for review_text in unlabeled_train["review"]:
+        sentences += review_to_sentences(review_text, tokenizer)
 
     # ****** Set parameters and train the word2vec model
 
@@ -153,11 +167,11 @@ if __name__ == '__main__':
 
     print "Creating average feature vecs for training reviews"
 
-    trainDataVecs = getAvgFeatureVecs(getCleanReviews(train), model, num_features)
+    trainDataVecs = get_avg_feature_vecs(get_clean_reviews(train), model, num_features)
 
     print "Creating average feature vecs for test reviews"
 
-    testDataVecs = getAvgFeatureVecs(getCleanReviews(test), model, num_features)
+    testDataVecs = get_avg_feature_vecs(get_clean_reviews(test), model, num_features)
 
 
     # ****** Fit a random forest to the training set, then make predictions
